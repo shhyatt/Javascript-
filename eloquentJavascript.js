8961250ed9ad611b9a5b141b0b028370eb995106
shhyatt <shhyatt@gmail.com>
2020-05-03 14:47:44 -0400

    FizzBuzz

--- eloquentJavascript.js
+++ eloquentJavascript.js
@@ -1,4 +1,15 @@
 for (let line = "#"; line.length < 8; line += "#")
   console.log(line);
 
+
+  function fizzBuzz(n){
+      if(n%15 === 0){
+          return "FizzBuzz";
+      }else if(n%3 === 0){
+          return "Fizz";
+      }else if(n%5 === 0){
+          return "Buzz";
+      }
+  }
+
   
