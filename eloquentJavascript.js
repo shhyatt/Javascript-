0d8fc27f795a38decf1be65c49f17b302894bebc
shhyatt <shhyatt@gmail.com>
2020-05-03 15:25:44 -0400

    chessboard

--- eloquentJavascript.js
+++ eloquentJavascript.js
@@ -12,4 +12,22 @@ for (let line = "#"; line.length < 8; line += "#")
       }
   }
 
-  
+
+  function chessBoard(){
+      let size = 8;
+
+  let board = "";
+
+  for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      if ((x + y) % 2 == 0) {
+        board += " ";
+      } else {
+        board += "#";
+      }
+    }
+    board += "\n";
+  }
+
+  console.log(board);
+  }
