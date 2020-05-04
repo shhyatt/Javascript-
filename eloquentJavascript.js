a69ad7d3352312dd06323efa69c51f55cc91fe49
shhyatt <shhyatt@gmail.com>
2020-05-04 15:28:16 -0400

    isEven function

--- eloquentJavascript.js
+++ eloquentJavascript.js
@@ -39,3 +39,15 @@ for (let line = "#"; line.length < 8; line += "#")
         return b;
     }
 }
+
+function isEven(n){
+    if(n === 0){
+        return true;
+    }else if(n === 1){
+        return false;
+    }else if(n<0){
+        return isEven(-n);
+    }else{
+        return isEven(n-2);
+    }
+}
