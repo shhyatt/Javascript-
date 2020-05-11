4be0d8dbd67c5c7cab94a1f9d8a6a9ab0f3303df
shhyatt <shhyatt@gmail.com>
2020-05-11 16:44:56 -0400

    twosum function

--- eloquentJavascript.js
+++ eloquentJavascript.js
@@ -123,3 +123,13 @@ var isPalindrome = function(x) {
     }else{
         return false;
     }
+
+  var twoSum = function(nums, target) {
+        for (i = 0; i < nums.length; i++){
+            for (j = i + 1; j < nums.length; j ++){
+               if (nums[i] + nums[j] == target){
+                   return [i, j]
+               }
+                }
+            }
+        }
