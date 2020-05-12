0b8b16d50ecae2caeb45c371ff6f50c7e2841912
shhyatt <shhyatt@gmail.com>
2020-05-12 13:30:25 -0400

    removeElement function

--- eloquentJavascript.js
+++ eloquentJavascript.js
@@ -133,3 +133,11 @@ var isPalindrome = function(x) {
                 }
             }
         }
+
+
+var removeElement = function(nums, val) {
+  while(nums.includes(val)){
+      nums.splice(nums.indexOf(val),1)
+  }
+       return nums.length
+}
